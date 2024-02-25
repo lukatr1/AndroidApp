80fb837939981a03bbfb5f568169bfb9c4d64794
lukatr1 <luka.trifunovic@stud.fh-campuswien.ac.at>
2024-02-25 20:08:36 +0100

    added MovieRow with few bugs

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,16 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +49,86 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Card {
-                        Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
-                    }
+                    MovieRow()
                 }
             }
+
         }
     }
 }
+
+// nr 1 add structure (page 15)
+@Composable
+fun MovieRow() {
+    Column {
+
+        Text("Column")
+        Box (
+            modifier = Modifier
+                .background(Color.LightGray)
+                //.border(1.dp, Color.Red)
+                .padding(bottom = 32.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.movie_image),
+                contentDescription = "placeholder_image",
+                modifier = Modifier
+                    .padding(7.dp)
+                    .clip(RoundedCornerShape(27.dp))
+            )
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = "likeBtn",
+                modifier = Modifier
+                    .size(27.dp)
+                    .align(Alignment.TopEnd)
+                    // add padding (don't know if this should be done this way)
+                    .offset(x = (-15).dp, y = 15.dp)
+                //.padding(8.dp)
+            )
+        }
+            Row (
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text("Row",
+                    modifier = Modifier
+                        .weight(1f)
+                )
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = "arrowUp",
+                    modifier = Modifier
+                        .size(24.dp)
+                )
+            }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
